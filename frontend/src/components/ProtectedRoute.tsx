087b7f9a42dab2db01e97c6f144458017ad85763
Chirag s <chirag748317@gmail.com>
2025-02-24 00:19:56 +0530

    auth

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,17 +1,21 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 
 const ProtectedRoute = () => {
   const { token } = useAuth();
-
-  console.log(" Checking token in ProtectedRoute:", token); 
-
   const tokenFromStorage = localStorage.getItem("token");
-  if (!token && !tokenFromStorage) {
-    console.warn(" Unauthorized access. Redirecting to login...");
+  const location = useLocation();
+
+  // Check if the user is coming from Google OAuth (e.g., `/auth/callback?google=true`)
+  const urlParams = new URLSearchParams(location.search);
+  const isGoogleLogin = urlParams.get("google");
+
+  console.log("Checking token in ProtectedRoute:", token);
+  
+  if (!token && !tokenFromStorage && !isGoogleLogin) {
+    console.warn("Unauthorized access. Redirecting to login...");
     return <Navigate to="/login" replace />;
   }
-  
 
   return <Outlet />;
 };
