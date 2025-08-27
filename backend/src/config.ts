b5b2ddc490adc43779e5eba60d8b3c0bf3ee4ea6
Chirag s <chirag748317@gmail.com>
2025-08-27 15:05:32 +0530

    updated google uri

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,4 +1,5 @@
-export const google_api="https://website-builder-9d8r.onrender.com"
-//export const google_api="http://localhost:3000"
+export const google_api = "https://sitecrafter-z54g.vercel.app"
+// Local dev URL
+// export const google_api = "http://localhost:3000"
 export const frontend_config="https://sitecrafter.vercel.app"
 //export const frontend_config="http://localhost:5173/"
